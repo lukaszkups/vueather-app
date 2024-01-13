6f78dffa63bc1e6cb844b2d14a3ffc7ab1452974
lukaszkups <lukaszkups@gmail.com>
2024-01-13 20:44:46 +0100

    Added first Cypress tests.

---
+++ cypress/e2e/rendering.cy.ts
@@ -0,0 +1,14 @@
+// https://on.cypress.io/api
+
+describe('Rendering test', () => {
+  it('visits the app root url', () => {
+    cy.visit('/');
+    cy.get('.main-widget');
+    cy.get('.main-widget .main-widget');
+  });
+
+  it('shows the title of default queried city', () => {
+    cy.visit('/');
+    cy.contains('h2.city-name', 'Poznań');
+  });
+});
